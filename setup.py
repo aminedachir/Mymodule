8e22dce89ee98adbb1ae6fe8e3584bc46107203b
aminedachir <dachiramine110@gmail.com>
2025-10-18 00:50:30 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Mymodule',
+    version='0.1',
+    py_modules=['mymodule'],
+    author='Your Name',
+    author_email='you@example.com',
+    description='A simple Python module',
+    url='https://github.com/aminedachir/Mymodule',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
